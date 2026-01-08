fa607bc755d89d419514623cdcbec33e16a204c9
Arya Doshi <arya.doshi@qubrid.com>
2026-01-08 19:50:27 +0530

    Update __init__.py

--- prompts/__init__.py
+++ prompts/__init__.py
@@ -1,4 +1,3 @@
-"""Prompts package initialization"""
-from .nutrition_prompt import FOOD_ANALYSIS_SYSTEM_PROMPT, DETAILED_NUTRITION_PROMPT
+from .nutrition_prompt import DETAILED_NUTRITION_PROMPT, CHAT_SYSTEM_PROMPT
 
-__all__ = ['FOOD_ANALYSIS_SYSTEM_PROMPT', 'DETAILED_NUTRITION_PROMPT']
+__all__ = ['DETAILED_NUTRITION_PROMPT', 'CHAT_SYSTEM_PROMPT']
