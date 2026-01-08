7c6fadc57b2c5126e5860320bcd633fe80c767b4
Arya Doshi <arya.doshi@qubrid.com>
2026-01-08 19:51:08 +0530

    Update __init__.py

--- utils/__init__.py
+++ utils/__init__.py
@@ -2,14 +2,22 @@
 from .api_client import call_qubrid_api, call_qubrid_api_stream
 from .image_processor import encode_image_to_base64
 from .parser import parse_nutrition_data
-from .ui_components import display_nutrition_card, display_health_score, display_dietary_badges
+from .ui_components import (
+    display_macro_row, 
+    display_health_bar, 
+    display_metrics_footer, 
+    format_analysis_report
+)
+from .styles import get_custom_css
 
 __all__ = [
     'call_qubrid_api',
     'call_qubrid_api_stream',
     'encode_image_to_base64',
     'parse_nutrition_data',
-    'display_nutrition_card',
-    'display_health_score',
-    'display_dietary_badges'
+    'display_macro_row',
+    'display_health_bar',
+    'display_metrics_footer',
+    'format_analysis_report',
+    'get_custom_css'
 ]
